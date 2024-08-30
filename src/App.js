4c95d4580ea923ce62c6a65501b149c17cbd7e88
Cristian Andres Valenzuela <tc.cvalenzuela@gmail.com>
2024-08-29 23:03:25 -0400

    limpieza archivos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/HeroSection';
-// import Servicios from './components/services/Servicios';
-// import HeroSection from './components/HeroSection';
-// import AboutUs from './components/AboutUs';
-// import Services from './components/Services';
-// import FeaturedProjects from './components/FeaturedProjects';
-// import Brands from './components/Brands';
-// import ContactForm from './components/ContactForm';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Servicios from './components/services/Servicios';
